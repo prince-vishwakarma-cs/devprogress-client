4792b4ad950f555832f66870acb55937d1a4c580
prince-vishwakarma-cs <pv913177@gmail.com>
2025-02-18 16:40:53 +0530

    fixed

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  server: {
+    port: 5173,
+  },
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+  },
 });
